9b12777e019f95b4a8fd9d252f68262a6f68459b
raj <pdjango34@gmail.com>
2024-10-18 14:07:10 +0530

    Added testcases

---
+++ math_operations.py
@@ -0,0 +1,19 @@
+# math_operations.py
+
+def add(a, b):
+    """
+    Function to add two numbers.
+    :param a: First number
+    :param b: Second number
+    :return: Sum of a and b
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Function to subtract two numbers.
+    :param a: First number
+    :param b: Second number
+    :return: Difference of a and b
+    """
+    return a - b
---
+++ test_index.py
@@ -0,0 +1,14 @@
+# test_math_operations.py
+
+import pytest
+from math_operations import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(10, 4) == 6
+    assert subtract(0, 0) == 0
